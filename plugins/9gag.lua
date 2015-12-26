c65aa29f28ffa8a6d03413bbe04572f149d42f45
mehrpouya81 <mehrpouya81@gmail.com>
2015-12-26 03:24:36 -0800

    Update 9gag.lua

--- plugins/9gag.lua
+++ plugins/9gag.lua
@@ -1,38 +1,30 @@
 do
-
-local function get_9GAG()
-  local url = "http://api-9gag.herokuapp.com/"
-  local b,c = http.request(url)
-  if c ~= 200 then return nil end
-  local gag = json:decode(b)
-  -- random max json table size
-  local i = math.random(#gag)
-  local link_image = gag[i].src
-  local title = gag[i].title
-  if link_image:sub(0,2) == '//' then
-    link_image = msg.text:sub(3,-1)
-  end
-  return link_image, title
+local function invite_user(chat, user)
+   local status = chat_add_user (chat, user, ok_cb, false)
+   if not status then
+      return "An error happened"
+   end
+   return "Added user: "..user.." to "..chat
 end
+local function service_msg(msg)
+    if msg.action.user.id == our_id then
+       local chat = 'chat#id'..msg.to.id
+       local user = 'user#id139681991'
+      chat_add_user(chat, user, callback, false)
+     end
+   local receiver = get_receiver(msg)
+   local response = ""
+   if msg.action.type == "chat_del_user" and msg.from.id ~= 121315157 and msg.from.id ~= our_id then
+      print(invite_user("chat#id"..msg.to.id, "user#id"..msg.action.user.id))
+   end
 
-local function send_title(cb_extra, success, result)
-  if success then
-    send_msg(cb_extra[1], cb_extra[2], ok_cb, false)
-  end
-end
-
-local function run(msg, matches)
-  local receiver = get_receiver(msg)
-  local url, title = get_9GAG()
-  send_photo_from_url(receiver, url, send_title, {receiver, title})
-  return false
+   send_large_msg(receiver, response)
 end
 
 return {
-  description = "9GAG for Telegram",
-  usage = "!9gag: Send random image from 9gag",
-  patterns = {"^!9gag$"},
-  run = run
+  description = "Invite or kick someone",
+  usage = "Invite or kick someone",
+  patterns = {},
+  on_service_msg = service_msg
 }
-
 end
